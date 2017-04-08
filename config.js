94d518b7f5ea59ed88228b8853ed6530c7200b1f
David S. Jackson <deepbsd@yahoo.com>
2017-04-08 19:07:26 -0400

    changed config.js for heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blogapp';
+                      'mongodb://deepbsd:2D33p4m3@ds035806.mlab.com:35806/blogapp';
 exports.PORT = process.env.PORT || 8080;
